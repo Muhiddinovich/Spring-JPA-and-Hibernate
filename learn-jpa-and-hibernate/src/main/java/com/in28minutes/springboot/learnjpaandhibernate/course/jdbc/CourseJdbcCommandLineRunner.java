5ce8e3df68c6fdba30ce95301c29a210f01ea207
Fahriddin <Fahriddin@WIN-U2794NDE3MO>
2024-05-24 06:09:09 -0400

    Querying data using Spring JDBC

--- learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
+++ learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -14,10 +14,13 @@ public class CourseJdbcCommandLineRunner implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		repository.insert(new Course(1,"Learn AWS Now!", "in28minutes"));
-		repository.insert(new Course(2,"Learn Azure Now!", "in28minutes"));
-		repository.insert(new Course(3,"Learn DevOps Now!", "in28minutes"));
+		repository.insert(new Course(1, "Learn AWS Now!", "in28minutes"));
+		repository.insert(new Course(2, "Learn Azure Now!", "in28minutes"));
+		repository.insert(new Course(3, "Learn DevOps Now!", "in28minutes"));
 		repository.deleteById(1);
+
+		System.out.println(repository.findById(2));
+		System.out.println(repository.findById(3));
 	}
 
 }
