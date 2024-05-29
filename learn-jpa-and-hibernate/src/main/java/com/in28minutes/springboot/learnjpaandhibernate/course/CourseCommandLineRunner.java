bc7e7e042c099e8732d7ac468e6dcbcfe8714c7f
Fahriddin <Fahriddin@WIN-U2794NDE3MO>
2024-05-29 05:14:24 -0400

    Spring Data JPA

--- learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
+++ learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 
 import com.in28minutes.springboot.learnjpaandhibernate.course.Course;
 import com.in28minutes.springboot.learnjpaandhibernate.course.jdbc.CourseJdbcRepository;
+import com.in28minutes.springboot.learnjpaandhibernate.course.springdatajpa.CourseSpringDataJpaRepository;
 
 @Component
 public class CourseCommandLineRunner implements CommandLineRunner {
@@ -13,18 +14,22 @@ public class CourseCommandLineRunner implements CommandLineRunner {
 //	@Autowired
 //	private CourseJdbcRepository repository;
 
+//	@Autowired
+//	private CourseJpaRepository repository;
+
 	@Autowired
-	private CourseJpaRepository repository;
+	private CourseSpringDataJpaRepository repository;
 
 	@Override
 	public void run(String... args) throws Exception {
-		repository.insert(new Course(1, "Learn AWS Jpa!", "in28minutes"));
-		repository.insert(new Course(2, "Learn Azure Jpa!", "in28minutes"));
-		repository.insert(new Course(3, "Learn DevOps Jpa!", "in28minutes"));
-		repository.deleteById(1);
+		repository.save(new Course(1, "Learn AWS Jpa!", "in28minutes"));
+		repository.save(new Course(2, "Learn Azure Jpa!", "in28minutes"));
+		repository.save(new Course(3, "Learn DevOps Jpa!", "in28minutes"));
+		repository.deleteById(1l);
 
-		System.out.println(repository.findById(2));
-		System.out.println(repository.findById(3));
+		System.out.println(repository.findById(2l));
+		System.out.println(repository.findById(3l));
+
 	}
 
 }
